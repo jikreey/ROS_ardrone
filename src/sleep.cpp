7ca69f0763dc981262c66b96b7e539a983146fcd
jikreey <dzikri.ugm@gmail.com>
2015-06-16 19:08:16 +0700

    Create sleep.cpp

---
+++ src/sleep.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello\n";
+  sleep(2);
+  cout << "World\n";
+  sleep(2);
+  cout << "!!!";
+  sleep(2);
+  cout << endl;
+
+  return 0;
+}
